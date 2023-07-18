8f6c2b89f481b0d2093607aa5f7d65dd18e618f9
0-daedalus <armageddon_WO@mail.ru>
2023-07-18 21:11:04 +0600

    Config package.json

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import Layout from '../components/layout'
+import UniversityCard from '../components/universityCard'
 import '../app/globals.css'
 
 export default function Home({universities}) {
   return (
     <Layout>
-      
+      <UniversityCard></UniversityCard>
     </Layout>
   )
 }
--- frontend/src/components/universityCard.jsx
+++ frontend/src/components/universityCard.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Image from "next/image";
+import { getUniversityData } from "@/lib/utils";
+
+export async function getStaticProps(){
+    const university = await getUniversityData();
+    return {
+        props: {
+            university
+        }
+    }
+}
 
 export default function UniversityCard({university}){
     return (
         <div className="card">
             <div className="card-body">
-                
+                {university}
             </div>
         </div>
     )
