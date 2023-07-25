875987bea73a5d99066600dc378e21aab65cbf63
0-daedalus <armageddon_WO@mail.ru>
2023-07-25 19:13:17 +0600

    Change image patterns

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     images: {
+      domains: ['lh3.googleusercontent.com', 'maps.googleapis.com'],
       remotePatterns: [
         {
           protocol: 'https',
