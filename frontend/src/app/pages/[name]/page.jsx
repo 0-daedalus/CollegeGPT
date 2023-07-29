cdcd7d99fa6b558e1c1b1acde555f899527d87d4
0-daedalus <armageddon_WO@mail.ru>
2023-07-29 19:04:38 +0600

    Add university pages to frontend

--- frontend/src/app/pages/[name]/page.jsx
+++ frontend/src/app/pages/[name]/page.jsx
@@ -10,8 +10,28 @@ import '@/app/globals.css'
 export default function UniversityPage({params}) {
     const name = decodeURI(params.name);
     const [token, setToken] = useState(null);
+    const [uni, setUni] = useState(null);
     useEffect(() => {
-        setToken(localStorage.getItem("token"));
+        const temp = localStorage.getItem("token");
+        setToken(temp);
+        const getUniversity = async (token) => {
+            //console.log(token);
+            const config = {
+                headers: {
+                    "accept": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                }
+            }
+            const data = {
+                name: name,
+            }
+            const res = await axios.get(`https://collegegpt-backend.onrender.com/stats/universities/${encodeURI(name)}`, config).catch((err) => console.log(err));
+            return res;
+        }
+        getUniversity(temp).then((res) => {
+            setUni(res.data);
+            //console.log(res);
+        });
     }, []);
     return (
         <>
@@ -19,7 +39,15 @@ export default function UniversityPage({params}) {
                     <title>{name}</title>
             </Head>
             <Layout>
-                <h1>Hello {decodeURI(params.name)}!</h1>
+                {uni ? (
+                    <div className="flex flex-col justify-center items-center gap-4">
+                        <img src={uni.imageUrl} alt="University Image" />
+                        <h1>{uni.name}</h1>
+                        <h2>{uni.type}</h2>
+                        <h3>{uni.description}</h3>
+                        <h4>{uni.tips}</h4>
+                    </div>
+                ) : null}
             </Layout>
         </>
     )
