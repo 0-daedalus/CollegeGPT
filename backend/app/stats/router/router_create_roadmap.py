d819924b6269ca923000f9d0b535e8b83b8b67ec
0-daedalus <armageddon_WO@mail.ru>
2023-08-03 15:40:22 +0600

    Update roadmap generation to include country 2

--- backend/app/stats/router/router_create_roadmap.py
+++ backend/app/stats/router/router_create_roadmap.py
@@ -17,13 +17,14 @@ def create_roadmap(
     svc: Service = Depends(get_service),
 ):
     stats = svc.repository.get_stats(jwt_data.user_id)
+    oldstats = svc.repository.getOldStats(jwt_data.user_id)
     if stats is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="No stats found for this user.",
         )
     curr = svc.repository.get_roadmap(jwt_data.user_id, data.university_name)
-    if curr is not None:
+    if curr is not None and oldstats == stats:
         return curr
     roadmap = svc.ai_svc.generate_roadmap(data.university_name, stats)
     roadmap_json = svc.ai_svc.generate_roadmap_json(roadmap)
